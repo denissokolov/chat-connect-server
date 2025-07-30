5a562c7e9a5ccaaed0286c36d1611c9e1f70fa1f
denissokolov <dnssokolov@gmail.com>
2025-07-30 16:30:56 +0200

    init project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Chat Connect Server
+
+## Prerequisites
+
+- Node.js (version 22 or higher)
+- npm package manager
+
+## Development
+
+Install dependencies
+
+```bash
+npm install
+```
+
+Run local server
+
+```bash
+npm run start:dev
+```
+
+Run all project checks
+
+```bash
+npm run check
+```
+
+## Production
+
+```bash
+# Install project dependencies
+$ npm ci
+
+# Run app in production mode
+$ npm run start:prod
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'coverage'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 'error',
+      'no-console': 'error',
+      'require-await': 'error',
+      'no-param-reassign': 'error',
+      'no-return-await': 'error',
+      'no-redeclare': 'error',
+      'no-shadow': 'error',
+    },
+  },
+)
